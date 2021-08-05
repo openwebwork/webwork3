cde3331dcf093d3b03c012821849558ce120657e
Peter Staab <peter.staab@gmail.com>
2021-08-05 09:14:18 -0400

    updates to user db functionality

--- lib/DB/Exception.pm
+++ lib/DB/Exception.pm
@@ -99,7 +99,7 @@ use Exception::Class (
 DB::Exception::UndefinedCourseField->Trace(1);
 DB::Exception::InvalidCourseField->Trace(1);
 # DB::Exception::UserNotInCourse->Trace(1);
-# DB::Exception::UserNotFound->Trace(1);
+DB::Exception::UserNotFound->Trace(1);
 # DB::Exception::CourseNotFound->Trace(1);
 DB::Exception::InvalidParameter->Trace(1);
 DB::Exception::UndefinedParameter->Trace(1);
