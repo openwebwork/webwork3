97d270ef6a773be68b5c2b8d3681a42f86d91f9d
Glenn Rice <grice1@missouriwestern.edu>
2021-09-07 14:16:05 -0500

    Update the mathjax configuration to add in some of the other needed MathJax configuration that was missed.  Also, instead of disabling all of eslint, just disable typescript rules.  This file should probably be changed to a js file.  I have upcoming changes to the eslint configuaration that will not run typescript rules on js files that will make this work correctly.
    Also, load mathajx files from the already installed npm package rather
    than using a cdn.  Furthermore, install the mathjax files for the
    production build.
    
    Also change formData.append to formData.set.

--- src/components/common/Problem.vue
+++ src/components/common/Problem.vue
@@ -1,8 +1,10 @@
 <template>
-	<div v-html="html" />
+	<div ref="renderDiv" v-html="html" />
 </template>
+
 <script lang="ts">
-import { defineComponent, Ref, ref, watch } from 'vue';
+import type { Ref } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
 import axios from 'axios';
 
 import './mathjax-config';
@@ -11,7 +13,7 @@ import 'mathjax-full/es5/tex-chtml.js';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface Window {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-  MathJax: any
+	MathJax: any
 }
 
 export default defineComponent({
@@ -25,36 +27,36 @@ export default defineComponent({
 	setup(props){
 		const html: Ref<string> = ref('');
 		const _file: Ref<string> = ref(props.file);
+		const renderDiv = ref<HTMLElement>();
 
- 		async function loadProblem() {
+		async function loadProblem() {
 			const formData = new FormData();
 			formData.set('problemSeed', '1');
 			formData.set('sourceFilePath', _file.value);
 			formData.set('outputFormat', 'static');
 
-			const response = await axios.post('/renderer/render-api', formData,
-				{ headers: { 'Content-Type': 'multipart/form-data' } });
+			let value;
+			try {
+				const response = await axios.post('/renderer/render-api', formData,
+					{ headers: { 'Content-Type': 'multipart/form-data' } });
 
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-			let value = response.data.renderedHTML as string;
-
-			// extract only the form element.
-			const match = /<form(.*)>(.*)<\/form>/s.exec(value);
-
-			// replace the script tags with \( \) or \[ \]
-			if (match) {
-				html.value = match[0];
-				// const m0 = match[0].replace(/<script type="math\/tex mode=display">(.+?)<\/script>/g, '\\[$1\\]');
-				// html.value = m0.replace(/<script type="math\/tex">(.+?)<\/script>/g, '\\($1\\)');
+				value = (response.data as { renderedHTML: string }).renderedHTML;
+			} catch(e) {
+				return;
 			}
-			void Promise.resolve()
-				.then(() => {
-					/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
-					window.MathJax.typesetPromise();
-					/* eslint-enable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
-				});
-			// .catch();  // need to figure out a more robust way of handling an error like this.
 
+			if (!value) return;
+
+			// Extract only the form element.
+			const match = /<form(.*)>(.*)<\/form>/s.exec(value);
+			if (match) html.value = match[0];
+
+			/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+			if (window.MathJax && typeof window.MathJax.typesetPromise == 'function') {
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+				window.MathJax.startup.promise.then(() => window.MathJax.typesetPromise([renderDiv.value]));
+			}
+			/* eslint-enable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
 		}
 
 		watch(() => props.file, () => {
@@ -63,7 +65,8 @@ export default defineComponent({
 		});
 
 		return {
-			html
+			html,
+			renderDiv
 		};
 	}
 });
--- src/components/common/mathjax-config.ts
+++ src/components/common/mathjax-config.ts
@@ -1,38 +1,47 @@
-/* eslint-disable */
+/* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call */
 
 interface Window {
-  MathJax: any
+	MathJax: any
 }
 
 window.MathJax = {
+	tex: {
+		packages: { '[+]': ['noerrors'] }
+	},
 	loader: {
 		paths: {
-			mathjax: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5'
+			mathjax: `${process.env.VUE_ROUTER_BASE ?? ''}mathjax`
+		},
+		load: ['input/asciimath', '[tex]/noerrors']
+	},
+	startup: {
+		ready: () => {
+			const AM = window.MathJax.InputJax.AsciiMath.AM;
+			for (let i = 0; i < AM.symbols.length; ++i) {
+				if (AM.symbols[i].input == '**') {
+					AM.symbols[i] = { input: '**', tag: 'msup', output: '^', tex: null, ttype: AM.TOKEN.INFIX };
+				}
+			}
+			return window.MathJax.startup.defaultReady();
 		}
 	},
 	options: {
 		renderActions: {
-			findScript: [10, function (doc: any) {
-				document.querySelectorAll('script[type^="math/tex"]').forEach(function(node) {
-					const nodeType = node.getAttribute('type');
-
-					const display = nodeType && !!nodeType.match(/; *mode=display/);
+			findScript: [10, (doc: any) => {
+				document.querySelectorAll('script[type^="math/tex"]').forEach((node: Element) => {
+					const display = !!/; *mode=display/.exec((node as HTMLScriptElement).type);
 					const math = new doc.options.MathItem(node.textContent, doc.inputJax[0], display);
 					const text = document.createTextNode('');
-					if (node.parentNode) {
-						node.parentNode.replaceChild(text, node);
-						math.start = {node: text, delim: '', n: 0};
-						math.end = {node: text, delim: '', n: 0};
-						doc.math.push(math);
-					}
+					node.parentNode?.replaceChild(text, node);
+					math.start = { node: text, delim: '', n: 0 };
+					math.end = { node: text, delim: '', n: 0 };
+					doc.math.push(math);
 				});
 			}, '']
-		}
+		},
+		ignoreHtmlClass: 'tex2jax_ignore'
 	}
-	// tex: {
-	// inlineMath: [
-	// ['$', '$'],
-	// ['\\(', '\\)']
-	// ]
-	// }
+
 };
