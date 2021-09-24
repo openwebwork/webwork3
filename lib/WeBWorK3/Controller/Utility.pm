87cc5f81895cede7078b432c19acf663fd01645a
K. Andrew Parker <kparker@citytech.cuny.edu>
2021-09-24 10:08:51 -0700

    backend route for receiving frontend logs

---
+++ lib/WeBWorK3/Controller/Utility.pm
@@ -0,0 +1,30 @@
+package WeBWorK3::Controller::Utility;
+use warnings;
+use strict;
+
+use Mojo::Base 'Mojolicious::Controller', -signatures;
+use Mojo::File qw(path);
+use Mojo::Log;
+use JSON qw(decode_json);
+
+my $path = path("$ENV{WW3_ROOT}/logs")->make_path->child('clientLog.log')->touch;
+my $clientLogFile = Mojo::Log->new(path => $path);
+
+sub clientLog($c) {
+	my $rawJSON = $c->req->body;
+	my $logEntry = decode_json($rawJSON);
+	my $level = ($logEntry->{level} =~ /(debug|info|warn|error)/) ? $1 : 'fatal' ;
+
+	# optionally log information from user cookie...
+	for my $cookie (@{$c->req->cookies}) {
+		$c->log->info($cookie->to_string);
+	}
+
+	# write entire json to file in production mode
+	$clientLogFile->$level($rawJSON);# if ( $ENV{MOJO_MODE} && $ENV{MOJO_MODE} eq 'production' );
+
+	$c->log->$level($logEntry->{message});
+	$c->rendered(200);
+};
+
+1;
