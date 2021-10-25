d6d0e1377f64a818f41a2eaf0cc072c877482ad3
Peter Staab <peter.staab@gmail.com>
2021-10-25 15:38:39 -0400

    change one more file for linting.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/* eslint-disable no-undef */
 
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
 // In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
 // which contains the path mapping (ie the `compilerOptions.paths` option):
+
 const { compilerOptions } = require('./tsconfig')
+
 
 
 module.exports = {
