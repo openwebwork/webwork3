ed6af69c0fe84022936601d29221a3480f67b9f6
K. Andrew Parker <kparker@citytech.cuny.edu>
2022-04-08 12:30:27 -0400

    add input field email string validation

--- src/components/admin/AddCourse.vue
+++ src/components/admin/AddCourse.vue
@@ -40,7 +40,8 @@
 						<div class="row q-col-gutter-lg">
 							<div class="col-6">
 								<input-with-blur outlined :disable="instructor_exists"
-									v-model="user.email" label="Email" />
+									v-model="user.email" label="Email"
+									:rules="[isValidEmail]"/>
 							</div>
 						</div>
 					</div>
@@ -212,6 +213,11 @@ export default defineComponent({
 					});
 			},
 			addCourse,
+			isValidEmail: (value: string) => {
+				const emailPattern =
+					/^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+				return emailPattern.test(value) || 'Invalid email';
+			}
 		};
 	}
 });
